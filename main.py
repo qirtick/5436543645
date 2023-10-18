b68e456b82c050f048dd94ded10a66c8edff08e4
MikhailAgafonov <144214080+qirtick@users.noreply.github.com>
2023-10-18 09:58:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1
+#my_list = [None, None, 1, [], (), {}, None]
+#
+#new_list = [i for i in my_list if i is not None]
+#
+#print(new_list)  # [1, [], (), {}]
+
+
+# 2
+#def list_insert(input_list, num, rep, start):
+    #result = input_list.copy()
+
+    #for i in range(rep):
+        #result.insert(start, num)
+    #return result
+#original_list = [1, 2, 3, 4, 5]
+#new_list = list_insert(original_list, 9, 2, 2)
+#print(new_list)
